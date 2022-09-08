bd22d8128d24b208fad7772ecc7a6e41b6be4958
HenrikCheng <48348033+HenrikCheng@users.noreply.github.com>
2022-09-08 21:25:37 +0200

    add-title-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Title from "./Components/Title";
 
 function App() {
   return (
     <div className="App">
+      <Title title="Tic-tac-toe" subtitle="Click an empty box to start" />
       <p className="text-red-500">Edit and save to reload.</p>
     </div>
   );
